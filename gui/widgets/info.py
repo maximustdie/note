22492d88730234cf15088d66bcb815f9ed0502ee
maximustdie <maximustdie@yandex.ru>
2022-12-12 12:17:57 +0300

    Создал класс окна оповещенния

---
+++ gui/widgets/info.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QMessageBox
+
+
+class InfoMessageBox:
+    def __init__(self, title, text):
+        self.msg = QMessageBox()
+        self.msg.setWindowTitle(title)
+        self.msg.setText(text)
+
+    def show(self):
+        self.msg.exec_()
