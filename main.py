47d4e6c34d6e5acccb5853f67ff6b15d17e76162
maximustdie <maximustdie@yandex.ru>
2023-11-06 14:29:43 +0300

    Довил создание папок для заметок при запуске приложения

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from gui.widgets.main_window import MainWindows
 if __name__ == '__main__':
     current_directory = os.getcwd()
     file_list = os.listdir(current_directory)
+    if not "files" in file_list:
+        os.mkdir("files")
+    if not "archive_files" in file_list:
+        os.mkdir("archive_files")
 
     app = QApplication(sys.argv)
     ex = MainWindows()
